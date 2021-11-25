499bcdd33dab1b7a04155e54cbda81aa159dfb77
AkshatShah214 <akshat@thinkbiz.co.in>
2021-11-25 16:42:37 +0530

    updated Settings.py file

--- crud_api/settings.py
+++ crud_api/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'crud_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'deeplearning',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost'
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deeplearning',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
