9e65756bd1e339d7be0cc7294de24b027e4c4545
AkshatShah214 <akshat@thinkbiz.co.in>
2021-11-25 15:14:47 +0530

    Update the database

--- crud_api/settings.py
+++ crud_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,8 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # # Activate Django-Heroku.
 # django_heroku.settings(locals())
 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 
 # PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
